b8a0c9e5ff36762e822a74c914f3c7f88db467dc
@seni <isolafri2003@yahoo.com>
2020-10-11 01:11:33 +0100

    Completed Code Quiz Website

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # code-quiz
 Test Players Knowledge on random programming questions.
+
+### Description
+Code Quiz: A typical coding assessment is a combination of multiple-choice questions and interactive coding challenges.
+
+
+## How to access?
+The website can be found at the following address: 
+
+
+## Screenshots:
+
+**Quiz start page.**
+!["Start Page"](/assets/mainPage.jpg?raw=true "Start page.")
+
+
+**Quiz complete page.**
+!["Start Page"](/assets/completePage.jpg?raw=true "Quiz completed page.")
+
+
+**Quiz sample question.**
+!["Start Page"](/assets/sampleQuestion.jpg?raw=true "Quiz completed page.")
+
+
+This project uses HTML, CSS, and Javascript. My application emphasizes the use of Javascript to provide quiz questions and collect user response to choose whether the Q&A's are correct, this then gives a score, and appends a results page of results from the user input data.
+
+
+This project has been deployed to GitHub Pages. To get this project up and running, you can follow the deployment link. Or, download the sources files to use this as a template.
+
+
+## Summary
+HTML and CSS and Javascript documents create a quiz with multiple choice questions with Javascript trivia
+This project utilizes the usage of appending HTML pages.
+
+
+## Psuedo code:
+Create a timer attached to a button with a starting value of 0
+When timer is pressed start a reverse countdown
+Start Quiz will be on appended page
+Append the question: choices
+When user selects the right answer, textcontent "Correct Answer!"
+When user selects the right answer, textcontent "Wrong Anwser!"
+Final score will keep track of how many the user got right
+Time left will be deducted from final score
+End Score Appended page
+local storage
+Travels to another HTML
+Retrieved Score result
+
+## Future_Improvements
+To add:
+- More quizzes
+- Randomize questions and choices.
+- A check to make sure user's score is in top 10 before adding it to localStorage.
+- More info in quiz-info-box, such as number of questions, and time alotted.
---
+++ javascripts/questions.js
@@ -0,0 +1,59 @@
+var questions = [
+  {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
+  {
+    title: "What is javascript used for:",
+    choices: ["To make website performace better.", "To make css look more beautiful", "To allow html to be standalone", "To make website interactive."],
+    answer: "To make website interactive."
+  },
+
+  {
+    title: "Which library uses the $ sign :",
+    choices: ["AJAX.", "AJAJ", "JQUERY", "JAVASCRIPT"],
+    answer: "Jquery."
+  },
+
+  {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      answer: "all of the above"
+  },
+  {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+  },
+  {
+      title: "What is a DOM in JavaScript?",
+      choices: ["Data of Mine", "Document of Master", "Data Object Modal", "Document Object Model"],
+      answer: "Document Object Model"
+  },
+  {
+      title: "The condition in an if/else statement is enclosed within ______.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+  },
+  {
+      title: "What is computer coding?,",
+      choices: ["A tv show", "a radio show", "Telling a computer what to do", "a list of functions"],
+      answer: "Telling a computer what to do"
+  },
+  {
+      title: "This programming language was the predecessor of Java, developed by James Gosling,",
+      choices: ["C++", "Oak", "Python"],
+      answer: "Oak"
+  },
+  {
+      title: "What language does the computer understands?",
+      choices: ["Binary/Machine Language", "Logic", "Code"],
+      answer: "Binary/Machine Language"
+  },
+  {
+      title: "What does CSS stand for?,",
+      choices: ["Cascading Style Sheet", "Calculating Solutions Sheet", "Computer String Sheet"],
+      answer: "Cascading Style Sheet"
+  },
+];
